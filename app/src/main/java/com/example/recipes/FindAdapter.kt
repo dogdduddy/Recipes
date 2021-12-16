273dddda59cb2d4036883acfc260b5e9385575fa
jeonbyeongseon <dogdduddy@gmail.com>
2021-12-17 03:05:09 +0900

    어댑터 연결중, map 사용중

--- app/src/main/java/com/example/recipes/FindAdapter.kt
+++ app/src/main/java/com/example/recipes/FindAdapter.kt
@@ -1,25 +1,54 @@
 package com.example.recipes
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import org.w3c.dom.Text
 
-class FindAdapter  : RecyclerView.Adapter<FindAdapter.ViewHolder>() {
+class FindAdapter(
+    private val recipeList: MutableList<Array<String>>,
+    private val context: Context,
+    private val firestoreDB: FirebaseFirestore)
+    : RecyclerView.Adapter<FindAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent!!.context).inflate(R.layout.item_find, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val recipe = recipeList[position]
+
+        holder!!.title.text = recipe[0]
+        holder.content.text = recipe[1]
+
+        //holder.edit.setOnClickListener { updateNote(recipe) }
+        //holder.delete.setOnClickListener { deleteNote(recipe.id!!, position) }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return recipeList.size
     }
 
     inner class ViewHolder internal constructor(view: View) : RecyclerView.ViewHolder(view) {
-        //Todo
+        internal var title: TextView
+        internal var content: TextView
+        internal var time: TextView
+        //internal var edit: ImageView
+        //internal var delete: ImageView
+
+        init {
+            title = view.findViewById(R.id.findTitle)
+            content = view.findViewById(R.id.findContent)
+            time = view.findViewById(R.id.findTime)
+            //edit = view.findViewById(R.id.ivEdit)
+            //delete = view.findViewById(R.id.ivDelete)
+        }
     }
 }
