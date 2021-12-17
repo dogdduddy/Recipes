357ff697953f8cd6cf0c8e661fc5af94169dd4a5
jeonbyeongseon <dogdduddy@gmail.com>
2021-12-17 14:59:31 +0900

    findWindow

--- app/src/main/java/com/example/recipes/MainActivity.kt
+++ app/src/main/java/com/example/recipes/MainActivity.kt
@@ -28,58 +28,24 @@ class MainActivity : AppCompatActivity() {
             val refs = database.collection("Recipes")
             // 검색 통해 나온 레시피 이름을 담는 배열
             val recipeList = mutableListOf<Array<String>>()
-            Log.d(TAG, "Pass 1 ")
             // 입력한 재료만큼 반복 (str.size-1)인 이유는 해당 반복문은 끝의 숫자를 포함하여 넣어주기 때문.
             for(i in 0..str.size-1) {
-                Log.d(TAG, "Pass 2 ")
-
-                if (ing_hash[str[i]] != null) {
-                    Log.d(TAG, "Pass 3 ")
-
-                    refs.document("까르보나라").get()
+                if(ing_hash[str[i].trim()]!=null) {
+                    refs.whereEqualTo(ing_hash[str[i].trim()].toString(), str[i].trim()).get()
                         .addOnSuccessListener { documents ->
-                            // 현재 {유제품=파마산치즈, 향신료=후추} 이렇게 나오는데
-                            // 파마산치즈, 후추를 뽑아낼 방법을 찾아보자
-                                // 방법이 있다면 재료라는 필드에 재료들 모두 map형태로 넣
-                            val a:Any? = documents.get("추가")
-                            if(a != null) {
-
-                                for(j in a){
-
-                                }
-                            }
-                            Log.d(TAG,"hashcode : "+a.toString())
-                            /*
+                            Log.d(TAG,documents.toString())
                             for (document in documents) {
                                 // 레시피 검색해서 나온 이름을 저장
-                                //recipeList.add(arrayOf(document.id.toString(), document.get("시").toString()))
+                                recipeList.add(arrayOf(document.id.toString(), document.get("시간").toString()))
                                 Log.d(TAG, document.id.toString())
                             }
-
-                             */
-                        }
+                        }git
                 }
-                /*
-                if(ing_hash[str[i]]!=null) {
-                    refs.whereEqualTo(ing_hash[str[i]].toString(), str[i]).get()
-                        .addOnSuccessListener { documents ->
-                            for (document in documents) {
-                                // 레시피 검색해서 나온 이름을 저장
-                                recipeList.add(arrayOf(document.id.toString(), document.get("시").toString()))
-                                // Log.d(TAG, document.id.toString())
-                            }
-                        }
-                }
-
-
             }
             adapter = FindAdapter(recipeList, applicationContext, database)
             binding.recyclerView.layoutManager = LinearLayoutManager(applicationContext)
             binding.recyclerView.itemAnimator = DefaultItemAnimator()
             binding.recyclerView.adapter = adapter
-
-                 */
-            }
         }
     }
 
